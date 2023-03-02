c7b10f19a3a1d8f87f558fd04b15861c313229a0
joaolalmeida <joaolalmeida20@gmail.com>
2023-03-02 16:16:01 -0300

    fundamentos-reactjs

--- 01_Fundamentos do ReactJS/01-fundamentos-reactjs/src/App.jsx
+++ 01_Fundamentos do ReactJS/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,18 @@
-function App() {
+//JSX = JavaScript + XML (HTML)
 
+//import Button from './Post'
+import {Post} from './Post';
+
+export function App() {
   return (
-    <h1>Hello World</h1>
+    //<h1>Teste</h1>
+    <div>
+      {/* Mais de um elemento precisa estar dentro de uma div por exemplo */}
+      < Post />
+      < Post />
+      < Post />
+    </div>
   )
 }
 
-export default App
+//export default App
---
+++ 01_Fundamentos do ReactJS/01-fundamentos-reactjs/src/Post.jsx
@@ -0,0 +1,10 @@
+export function Post() {
+  return <p>Post</p>
+}
+//export default Button
+
+
+//Default Exports vs Named Exports
+
+//Default Exports pode mudar o nome na hora da importação
+//Named exports é dada a exportacao junto com a funcao
