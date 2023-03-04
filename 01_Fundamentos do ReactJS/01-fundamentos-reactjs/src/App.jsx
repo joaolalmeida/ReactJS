4c5dc405ae61d52ed55f5e3abdb763d2ecd2fef3
joaolalmeida <joaolalmeida20@gmail.com>
2023-03-04 11:17:54 -0300

    componente-sidebar

--- 01_Fundamentos do ReactJS/01-fundamentos-reactjs/src/App.jsx
+++ 01_Fundamentos do ReactJS/01-fundamentos-reactjs/src/App.jsx
@@ -4,7 +4,10 @@
 import { Header } from './components/Header';
 import {Post} from './Post';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
@@ -12,14 +15,21 @@ export function App() {
     <div>
       <Header />
       {/* Mais de um elemento precisa estar dentro de uma div por exemplo */}
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi porro accusantium iusto repudiandae animi, tempore et perferendis harum recusandae officiis soluta, ratione maiores minus dignissimos non error eum, est reprehenderit."
-      />
-      <Post
-        author="Gabriel Buzzi"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi porro accusantium iusto repudiandae animi, tempore et perferendis harum recusandae officiis soluta, ratione maiores minus dignissimos non error eum, est reprehenderit."
-      />
+
+       <div className={styles.wrapper}> {/*Colocar no proprio APP para ficar melhor para incluir os modulos criados */}
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi porro accusantium iusto repudiandae animi, tempore et perferendis harum recusandae officiis soluta, ratione maiores minus dignissimos non error eum, est reprehenderit."
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi porro accusantium iusto repudiandae animi, tempore et perferendis harum recusandae officiis soluta, ratione maiores minus dignissimos non error eum, est reprehenderit."
+          />
+        </main>
+      </div>
     </div>
   )
 }
