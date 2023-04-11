2f060ceee6723da0b326ce7493856ff4230d7b68
joaolalmeida <joaolalmeida20@gmail.com>
2023-04-11 09:29:04 -0300

    icon

--- 01_Fundamentos do ReactJS/01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01_Fundamentos do ReactJS/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -4,6 +4,10 @@ import { Avatar } from "./Avatar"
 import styles from "./Comment.module.css"
 
 export function Comment({ content }) {
+  function handleDeleteComment() {
+    
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/joaolalmeida.png" alt="" />  {/* Criando uma proprierade chamada hasBorder para tirar a borda deste elemento em especifico */}
@@ -18,7 +22,7 @@ export function Comment({ content }) {
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
