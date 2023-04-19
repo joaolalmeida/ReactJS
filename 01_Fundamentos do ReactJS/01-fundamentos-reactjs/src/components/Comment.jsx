a1b767259f676e3aa0b7661a03f61568bdc55fbd
joaolalmeida <joaolalmeida20@gmail.com>
2023-04-19 12:16:39 -0300

    comunicacao entre componentes

--- 01_Fundamentos do ReactJS/01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01_Fundamentos do ReactJS/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -3,9 +3,10 @@ import { Avatar } from "./Avatar"
 
 import styles from "./Comment.module.css"
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
   function handleDeleteComment() {
-    
+
+    onDeleteComment(content)
   }
 
   return (
--- 01_Fundamentos do ReactJS/01-fundamentos-reactjs/src/components/Post.jsx
+++ 01_Fundamentos do ReactJS/01-fundamentos-reactjs/src/components/Post.jsx
@@ -43,6 +43,10 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value)
   }
 
+  function deleteComment(comment) {
+    console.log(`Deletar comentario ${comment}`)
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -96,7 +100,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment key={comment} content={comment} />
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment} //Chamando a funcao como propriedade para deletar um comentario
+            />
+          ) 
         })}
       </div>
     </article>
